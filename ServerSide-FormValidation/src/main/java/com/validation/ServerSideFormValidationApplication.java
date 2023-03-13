0cbea0b09d1506d4cd466740d734c3ab99f399cc
Rishabh Srivastava <rishabhchandra2408@gmail.com>
2023-03-13 23:16:24 +0530

    Base Structure Created

---
+++ ServerSide-FormValidation/src/main/java/com/validation/ServerSideFormValidationApplication.java
@@ -0,0 +1,13 @@
+package com.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideFormValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideFormValidationApplication.class, args);
+	}
+
+}
